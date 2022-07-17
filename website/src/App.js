b4ace07402c8f06214a3671c3b82d3a2b2a31a7a
Sebastian Melendez <sebastianmelendez@Sebastians-MacBook-Pro.local>
2022-07-17 12:25:56 -0400

    clean up

--- website/src/App.js
+++ website/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Cohort from './components/Cohort';
 import Footer from './components/Footer';
@@ -7,20 +6,6 @@ import Partners from './components/Partners';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Cohort/>
       <Partners/>
       <Footer/>
